43a91c83f8ced92550e4fe8e923893769ff197d0
djs <lambdan@users.noreply.github.com>
2022-03-18 16:16:16 +0100

    winner, failure, browser size change

--- ss.js
+++ ss.js
@@ -4,23 +4,34 @@ var img = new Image();
 
 var rows = 2;
 var cols = 5;
+
+var max_guesses = rows*cols;
 var guesses = 0;
-var lastclicked = ""; // for doube click prevention
+var lastclicked = ""; // for double click prevention
+var blockers = [];
 
 var gamesjson;
 var games = [];
+var game_titles = []; // will be sorted for the input box
 var game_today = ""; // title we're looking for today
+
+var game_over = false;
+var game_won = false;
 
 
 function init () {
 	// read and parse games.json
 	$.each(gamesjson.games, function(k,v) {
 		games.push(v);
-		$("#games").append('<option value="' + v.title + '">') // push game name to search box
+		game_titles.push(v.title);
 	});
 
-	r = Math.floor(Math.random() * games.length);
+	// push sorted list of games to the input box ("datalist")
+	$.each(game_titles.sort(), function(k,v) { 
+		$("#games").append('<option value="' + v + '">') // push game name to search box
+	});
 
+	r = Math.floor(Math.random() * games.length); // game for today picked here
 	game_today = games[r].title
 	todays_image = "screens/" + games[r].image
 	todays_aspect = games[r].aspect
@@ -38,6 +49,7 @@ function init () {
 	});
 
 
+	// setup the image and create blockers
 	img.src = todays_image
 	img.onload = function() {
 		// create blockers
@@ -47,33 +59,63 @@ function init () {
 		}
 		}	
 	}
-
 	$("#image").html(img)
 }
 
+function regenPlayfield() {
+	console.log("regenPlayfield")
+	$("#image").html("<p>Shit</p>")
+
+	$.each(blockers, function(k,v) {
+		$(v.id).remove();
+	});
+
+	// setup the image and create blockers
+	img.src = todays_image
+	img.onload = function() {
+		// create blockers
+		for (let r = 0; r < rows; r++) {
+			for (let c = 0; c < cols; c++) {
+			addBlock(r,c)
+		}
+		}	
+	}
+	$("#image").html(img)
+
+}
+
 function addBlock(row,col) {
-	console.log(img.width, img.height);
-	//console.log("adding block", row, col)
 	w = img.width/cols
 	h = img.height/rows
 
 	x = col * w
 	y = row * h
 
-	s = '<div id="blocker-' + row + '-' + col + '" class="blocker" style="margin-top:' + y + ';margin-left:' + x + ';width:' + w + ';height:' + h +';"></div>';
+	name = "blocker-" + row + "-" + col
+	s = '<div id="' + name + '" class="blocker" style="margin-top:' + y + ';margin-left:' + x + ';width:' + w + ';height:' + h +';"></div>';
 	$('#playfield').append(s)
+	blockers.push({"id": name, "active": true})
 }
 
 function clickedBlocker(id) {
 	revealSquare(id);
 	incrementGuesses();
-
-
 }
 
 function revealSquare(id) {
+	$.each(blockers, function(k,v) {
+		if (v.id == id) {
+			if (v.active) {
+				blockers[k].active = false
+			} else {
+				return false // blocker was not active
+			}
+		}
+	});
+
 	$("#" + id).fadeOut(1500, function() {
-		//console.log("done with fadeout")
+		// animation done
+		return true
 	});
 }
 
@@ -82,16 +124,57 @@ function incrementGuesses() {
 	$("#guesses-count").html(guesses);
 }
 
+function revealAll() {
+	$.each(blockers, function(k,v) {
+		if (v.active) {
+			revealSquare(v.id)
+		}
+	});
+}
+
 function makeGuess() {
+	if (game_over) {
+		return // game is over, do nothing
+	}
+
 	incrementGuesses();
 
 	var guess = $('#guess').val();
 
-	if (guess == game_today) {
-		alert("WINNER!")
-	} else {
-		alert("WRONG!")
+	if (guess == game_today) { // winner!
+		game_won = true;
+		game_over = true;
+
+		revealAll();
+		$("#guessinput").fadeOut(2000, function() {
+			$("#guessinput").html('<h2 class="winner">Winner!</h2><p>The game is: ' + game_today + '</p>')
+			$("#guessinput").fadeIn(2000);
+		});
+	} else if (guesses == max_guesses) { // failure
+		game_won = false;
+		game_over = true;
+
+		revealAll();
+		$("#guessinput").fadeOut(2000, function() {
+			$("#guessinput").html('<h2 class="failure">Failure.</h2><p>The game was: ' + game_today + '</p>')
+			$("#guessinput").fadeIn(2000);
+		});
+
+	} else { // just wrong guess
+		$("#playfield").effect("shake");
+		
+		// reveal one random tile
+		r = Math.floor(Math.random() * blockers.length)
+		while (blockers[r].active == false) {
+			r = Math.floor(Math.random() * blockers.length)
+		}
+		revealSquare(blockers[r].id)
+
 	}
+}
+
+function help() {
+	alert("Click a tile to reveal part of a screenshot and guess what game it is.\nEvery wrong guess reveals an additional tile.\n\nPlease avoid changing your browser size.")
 }
 
 $(window).on("load", function() {
@@ -107,7 +190,7 @@ $(window).on("load", function() {
     	//console.log( $(event)[0].target.id )
     	id = $(event)[0].target.id
     	if (id.includes("blocker-")) {
-    		if (lastclicked == id) {
+    		if (lastclicked == id || game_over) {
     			//console.log("blocking double click!!")
     		} else {
     			clickedBlocker(id);
@@ -119,6 +202,11 @@ $(window).on("load", function() {
 	});
 });
 
-$(window).resize(function() {
-	//TODO update blockers on resize
-})
+var wwidth = $(window).width();
+$(window).on('resize', function() {
+  if ($(this).width() !== wwidth) {
+    wwidth = $(this).width();
+    $("#playfield").hide();
+    $("#guessfield").html("<p>Browser size changed. Please reload page.</p>")
+  }
+});
